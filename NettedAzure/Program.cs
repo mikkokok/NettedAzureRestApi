87196d64f09430acc10c50e2c58b73071dba2b4b
Mikko Kokkonen <mikko.kokkonen@outlook.com>
2020-05-17 21:06:13 +0300

    Refactored to use httpclient

--- NettedAzure/Program.cs
+++ NettedAzure/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using NettedAzure.Config;
@@ -15,14 +18,25 @@ namespace NettedAzure
             var confLoader = new ConfigLoader();
             var config = confLoader.LoadConfig();
             var token = await GetAccessToken(config.TenantId, config.PrincipalId, config.PrincipalKey);
-            var response = JObject.Parse(GetSubscriptionResourceProvider(config.SubscriptionId, token));
-            var registrationState = response.SelectToken(Constants.RegistrationState);
-            if (registrationState.Value<string>() == Constants.NotRegistered || registrationState.Value<string>() == Constants.UnRegistered)
-            {
-                response = JObject.Parse(RegisterResourceProvider(config.SubscriptionId, token));
-                registrationState = response.SelectToken(Constants.RegistrationState);
-            }
+
+            RegisterProviderIfNotRegistered(config.SubscriptionId, token);
             Console.ReadLine();
+        }
+
+        private static void RegisterProviderIfNotRegistered(string subscriptionId, string token)
+        {
+            var response = JObject.Parse(GetSubscriptionResourceProvider(subscriptionId, token).Result.Content
+                .ReadAsStringAsync().Result);
+            var registrationState = response.SelectToken(Constants.RegistrationState);
+            if (registrationState.Value<string>() != Constants.NotRegistered &&
+                registrationState.Value<string>() != Constants.UnRegistered)
+            {
+                return;
+            }
+
+            response = JObject.Parse(RegisterResourceProviderAsync(subscriptionId, token).Result.Content
+                .ReadAsStringAsync().Result);
+            registrationState = response.SelectToken(Constants.RegistrationState);
         }
 
         private static async Task<string> GetAccessToken(string tenantId, string clientId, string clientSecret)
@@ -41,33 +55,34 @@ namespace NettedAzure
             return token;
         }
 
-        private static string GetSubscriptionResourceProvider(string subscriptionId, string token)
+        private static async Task<HttpResponseMessage> GetSubscriptionResourceProvider(string subscriptionId, string token)
         {
             var uri =
                 $"https://management.azure.com/subscriptions/{subscriptionId}/providers/Microsoft.OperationalInsights?api-version=2019-10-01";
 
-            return GetWebResponse(uri, token, "GET");
+            return await GetWebResponseAsync(uri, token, "GET");
         }
-        private static string RegisterResourceProvider(string subscriptionId, string token)
+        private static async Task<HttpResponseMessage> RegisterResourceProviderAsync(string subscriptionId, string token)
         {
             var uri =
                 $"https://management.azure.com/subscriptions/{subscriptionId}/providers/Microsoft.OperationalInsights/register?api-version=2019-10-01";
 
-            return GetWebResponse(uri, token, "POST");
+            return await GetWebResponseAsync(uri, token, "POST");
         }
 
-        private static string GetWebResponse(string uri, string token, string method)
+        private static async  Task<HttpResponseMessage> GetWebResponseAsync(string uri, string token, string method)
         {
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
-            httpWebRequest.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + token);
-            httpWebRequest.ContentType = "application/json";
-            httpWebRequest.Method = method;
+            var req = new HttpClient();
+            req.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream() ?? throw new InvalidOperationException()))
+            switch (method)
             {
-                return streamReader.ReadToEnd();
+                case "POST":
+                    return await req.PostAsync(uri, new StringContent("", Encoding.UTF8, "application/json"));
+                case "GET":
+                    return await req.GetAsync(uri);
+                default:
+                    throw new Exception("Unknow web method");
             }
         }
     }
